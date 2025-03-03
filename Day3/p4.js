a7b709b6691d399ab60f76f1fe561669bcd3dda8
ROHITMISHRA20411 <rohitmishra6052@gmail.com>
2025-03-03 15:30:50 +0530

    dataJSON

---
+++ Day3/p4.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs/promises')
+
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    if(req.url==='/getdata' && req.method==='GET')
+    {
+        const data = await fs.readFile('./data.json','utf8');
+        res.end(JSON.stringify(data));
+    }
+    else if(req.url==='/setdata' && req.method==='POST')
+    {
+        let body='';
+        req.on('data', chunk => {
+            body += chunk;
+        });
+        req.on('end', async () => {
+            let users=[];  
+            users=await fs.readFile('./data.json'); 
+            users=JSON.parse(users);
+            const newid=users.length>0?users[users.length-1].id+1:1;
+            const data = JSON.parse(body);
+            data.id = newid;
+            users.push(data);
+            await fs.writeFile('./data.json', JSON.stringify(users,null,2));
+            res.end(JSON.stringify({Message:'Data saved'}));
+        });    
+    }
+    else
+    {}    
+   
+        
+}) 
+
+server.listen(9000, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Server started at 9000');
+    }
+})    
